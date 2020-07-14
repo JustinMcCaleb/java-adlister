deb7af7b3a4522052c4a82ab0681d96c51b2297a
Justin McCaleb <justinleemccaleb@gmail.com>
2020-07-14 11:09:56 -0500

    Completed Servlets Exercises

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+
+        String name = req.getParameter("name");
+
+        if(name == null){
+            name = "World";
+        }
+        // -- /hello?name=Justin   -- would be the url/query string to output Justin as the name
+        out.println("<h1>Hello, " + name + "!</h1>");
 
     }
 }
