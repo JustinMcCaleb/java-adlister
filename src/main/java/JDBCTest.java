f525a35cf9fdb91795553cc742eb14a48ef3d36a
Justin McCaleb <justinleemccaleb@gmail.com>
2020-07-20 18:25:38 -0500

    completed first mini exercise from lecture

---
+++ src/main/java/JDBCTest.java
@@ -0,0 +1,88 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCTest {
+
+    private static List<String> getEmployeeFirstNames() {
+
+            List<String> firstNames = new ArrayList<>();
+
+        try {
+
+            //CREATING DRIVER AND CONNECTION OBJECTS
+            DriverManager.registerDriver(new Driver());
+
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/codeup_test_db?serverTimezone=UTC&useSSL=false",
+                    "root",
+                    "codeup"
+            );
+
+            //CREATING A STATEMENT OBJECT
+            Statement statement = conn.createStatement();
+
+            String query = "SELECT first_name FROM employees.employees LIMIT 10";
+
+            ResultSet rs = statement.executeQuery(query);
+
+
+            while(rs.next()){
+                firstNames.add(rs.getString(1));
+            }
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return firstNames;
+
+    }
+
+
+    private static List<String> getEmployeeFirstNames(int limit) {
+
+        List<String> firstNames = new ArrayList<>();
+
+        try {
+
+            //CREATING DRIVER AND CONNECTION OBJECTS
+            DriverManager.registerDriver(new Driver());
+
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/employees?serverTimezone=UTC&useSSL=false",
+                    "root",
+                    "codeup"
+            );
+
+            //CREATING A STATEMENT OBJECT
+            Statement statement = conn.createStatement();
+
+            String query = "SELECT first_name FROM employees.employees LIMIT " + limit;
+
+            ResultSet rs = statement.executeQuery(query);
+
+
+            while (rs.next()) {
+                firstNames.add(rs.getString(1));
+            }
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return firstNames;
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(getEmployeeFirstNames());
+        System.out.println(getEmployeeFirstNames(12));
+    }
+
+}
