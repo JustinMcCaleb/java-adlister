4fc6860d605e2df7dcf7dfa7aba1abd0b6ebe0a6
Justin McCaleb <justinleemccaleb@gmail.com>
2020-07-21 18:36:18 -0500

    completed JDBC exercise round 2

---
+++ src/main/java/database/migration.sql
@@ -0,0 +1,21 @@
+USE adlister_practice_db;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS ads;
+
+CREATE TABLE users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(250) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
