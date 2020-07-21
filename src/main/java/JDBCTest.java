d1e813caec6b2e426c03fb21864aa7f8c5959bc3
Justin McCaleb <justinleemccaleb@gmail.com>
2020-07-20 19:38:06 -0500

    added update album functionality to main method

--- src/main/java/JDBCTest.java
+++ src/main/java/JDBCTest.java
@@ -83,6 +83,57 @@ public class JDBCTest {
     public static void main(String[] args) {
         System.out.println(getEmployeeFirstNames());
         System.out.println(getEmployeeFirstNames(12));
+
+
+        try {
+
+            //CREATING DRIVER AND CONNECTION OBJECTS
+            DriverManager.registerDriver(new Driver());
+
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/codeup_test_db?serverTimezone=UTC&useSSL=false",
+                    "root",
+                    "codeup"
+            );
+
+            //CREATING A STATEMENT OBJECT
+            Statement statement = conn.createStatement();
+
+            Album updateAlbum = new Album(
+                4,
+                "Magic Sword",
+                "Volume 1",
+                 2016,
+                 15.0,
+                 "Synthwave"
+            );
+
+//            String updateQuery = String.format("UPDATE albums SET artist = '%s', name = '%s', releaseDate = '%d', sales = '%f', genre = '%s' WHERE id = '%d',",
+//                    updateAlbum.getArtist(),
+//                    updateAlbum.getName(),
+//                    updateAlbum.getReleaseDate(),
+//                    updateAlbum.getSales(),
+//                    updateAlbum.getGenre(),
+//                    updateAlbum.getId()
+//            );
+
+            String updateQuery = String.format("UPDATE albums SET artist = '%s', name = '%s', release_date = %d, sales = %f, genre = '%s' WHERE id = %d",
+                    updateAlbum.getArtist(),
+                    updateAlbum.getName(),
+                    updateAlbum.getReleaseDate(),
+                    updateAlbum.getSales(),
+                    updateAlbum.getGenre(),
+                    updateAlbum.getId()
+            );
+
+            statement.executeUpdate(updateQuery);
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
 
-}
+
+    }
